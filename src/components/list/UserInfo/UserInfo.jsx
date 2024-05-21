9a799f8371be35171fe296a4ca3efe68127fa1dc
hamitLicina <hamitlicina@gmail.com>
2024-05-21 17:21:49 +0300

    Some little Css changes

--- src/components/list/UserInfo/UserInfo.jsx
+++ src/components/list/UserInfo/UserInfo.jsx
@@ -4,20 +4,20 @@ import './UserInfo.css'
 
 
 const UserInfo = () => {
-  return (
-    <div className='userInfo'>
-        <div className="user">
-            
-        <img src='./avatar.png' alt='' />
-        <h2>Hamit Licina</h2>
+    return (
+        <div className='userInfo'>
+            <div className="user">
+
+                <img src='./avatar.png' alt='' />
+                <h2>Hamit Licina</h2>
+            </div>
+            <div className="icons">
+                <img src='./more.png' alt='' />
+                <img src='./video.png' alt='' />
+                <img src='./edit.png' alt='' />
+            </div>
         </div>
-        <div className="icons">
-            <img src='./more.png' alt='' />
-            <img src='./video.png' alt='' />
-            <img src='./edit.png' alt='' />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default UserInfo

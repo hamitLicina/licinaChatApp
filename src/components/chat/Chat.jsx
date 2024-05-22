2d4447efefee7b49b2428c2b857e424135d88ab2
hamitLicina <hamitlicina@gmail.com>
2024-05-22 17:06:38 +0300

    Update App and Chat component styles, add birdie.jpg asset

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -31,7 +31,46 @@ const Chat = () => {
           <img src='./info.png' alt='' />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Baby I love you so much! More than ever...</p>
+            <span>1 min Ago</span>
+          </div>
+        </div>
+        
+        <div className="message own">
+          <div className="texts">
+            <p>Baby I love you so much! More than ever...</p>
+            <span>1 min Ago</span>
+          </div>
+        </div>
+        
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Baby I love you so much! More than ever...</p>
+            <span>1 min Ago</span>
+          </div>
+        </div>
+        
+        <div className="message own">
+          <div className="texts">
+            <p>Baby I love you so much! More than ever...</p>
+            <span>1 min Ago</span>
+          </div>
+        </div>
+
+        <div className="message">
+          <img src="./birdie.jpg" alt="" />
+          <div className="texts">
+            <p>Baby I love you so much! More than ever...</p>
+            <span>1 min Ago</span>
+          </div>
+        </div>
+        
+      </div>
       <div className="bottom">
         <div className="icons">
           <img src="./img.png" alt="" />

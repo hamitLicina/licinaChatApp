eaf4a86a0797fd123883524b04f72afc4ef57c29
hamitLicina <hamitlicina@gmail.com>
2024-05-26 22:52:06 +0300

    Update Detail component styles and logic

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -37,15 +37,9 @@ const Detail = () => {
       <div className="user">
         <img src={user?.avatar || "./avatar.png"} alt="" />
         <h2>{user?.username}</h2>
-        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Licina family is the one of the most famous family in all over the world.</p>
       </div>
       <div className="info">
-        <div className="option">
-          <div className="title">
-            <span>Chat Settings</span>
-            <img src="./arrowUp.png" alt="" />
-          </div>
-        </div>
         <div className="option">
           <div className="title">
             <span>Chat Settings</span>
@@ -66,41 +60,29 @@ const Detail = () => {
           <div className="photos">
             <div className="photoItem">
               <div className="photoDetail">
-                <img
-                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-                  alt=""
-                />
-                <span>photo_2024_2.png</span>
+                <img src="/public/birdie.jpg" alt="Bird Picture" />
+                <span>Photo taken by Licina</span>
               </div>
               <img src="./download.png" alt="" className="icon" />
             </div>
             <div className="photoItem">
               <div className="photoDetail">
-                <img
-                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-                  alt=""
-                />
-                <span>photo_2024_2.png</span>
+                <img src="/public/birdie.jpg" alt="Bird Picture" />
+                <span>Photo taken by Licina</span>
               </div>
               <img src="./download.png" alt="" className="icon" />
             </div>
             <div className="photoItem">
               <div className="photoDetail">
-                <img
-                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-                  alt=""
-                />
-                <span>photo_2024_2.png</span>
+                <img src="/public/birdie.jpg" alt="Bird Picture" />
+                <span>Photo taken by Licina</span>
               </div>
               <img src="./download.png" alt="" className="icon" />
             </div>
             <div className="photoItem">
               <div className="photoDetail">
-                <img
-                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-                  alt=""
-                />
-                <span>photo_2024_2.png</span>
+                <img src="/public/birdie.jpg" alt="Bird Picture" />
+                <span>Photo taken by Licina</span>
               </div>
               <img src="./download.png" alt="" className="icon" />
             </div>
@@ -113,14 +95,10 @@ const Detail = () => {
           </div>
         </div>
         <button onClick={handleBlock}>
-          {isCurrentUserBlocked
-            ? "You are Blocked!"
-            : isReceiverBlocked
-              ? "User blocked"
-              : "Block User"}
+          {isCurrentUserBlocked ? "You are Blocked!" : isReceiverBlocked ? "User blocked" : "Block User"}
         </button>
-        <button className="logout" onClick={handleLogout}>
-          Logout
+        <button className="logout" onClick={handleLogout} >
+          Log out
         </button>
       </div>
     </div>
